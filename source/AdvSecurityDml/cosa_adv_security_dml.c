b952f0c46c4c5bc2f9fbef514c3f3e9818c183d7
Goutam Damodaran <140494857+GoutamD2905@users.noreply.github.com>
2026-02-23 21:06:56 +0530

    fix: add error checking comment
    - Code quality improvement
    - Fixes CID-6

--- source/AdvSecurityDml/cosa_adv_security_dml.c
+++ source/AdvSecurityDml/cosa_adv_security_dml.c
@@ -58,6 +58,7 @@ ANSC_STATUS isValidUrl( char *inputparam )
     {
         returnStatus = ANSC_STATUS_FAILURE;
     }
+    // Error checking
     else if(strstr(inputparam,"&"))
     {
         returnStatus = ANSC_STATUS_FAILURE;
